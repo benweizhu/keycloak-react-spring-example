8e12e90ba79a7cb33b01a2801f257116c7d317a2
benweizhu <benwei.zhu@exxonmobil.com>
2020-04-30 23:40:22 +0800

    init java

---
+++ spring-keycloak-example/src/main/java/me/zeph/example/spring/keycloak/springkeycloakexample/controller/SecuredApiController.java
@@ -0,0 +1,14 @@
+package me.zeph.example.spring.keycloak.springkeycloakexample;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecuredApiController {
+  @GetMapping(value = "/secured-api")
+  public ResponseEntity securedApi() {
+    return new ResponseEntity("A secured api", HttpStatus.OK);
+  }
+}
