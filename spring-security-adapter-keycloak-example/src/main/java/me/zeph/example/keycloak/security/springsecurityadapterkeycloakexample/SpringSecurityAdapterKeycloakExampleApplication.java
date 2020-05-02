6bb7cb07a80a3b87c35aed20155881ff77e43505
benweizhu <benwei.zhu@exxonmobil.com>
2020-05-02 09:35:21 +0800

    add spring-security-adapter-keycloak-example

---
+++ spring-security-adapter-keycloak-example/src/main/java/me/zeph/example/keycloak/security/springsecurityadapterkeycloakexample/SpringSecurityAdapterKeycloakExampleApplication.java
@@ -0,0 +1,13 @@
+package me.zeph.example.keycloak.security.springsecurityadapterkeycloakexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAdapterKeycloakExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSecurityAdapterKeycloakExampleApplication.class, args);
+  }
+
+}
---
+++ spring-security-adapter-keycloak-example/src/test/java/me/zeph/example/keycloak/security/springsecurityadapterkeycloakexample/SpringSecurityAdapterKeycloakExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zeph.example.keycloak.security.springsecurityadapterkeycloakexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAdapterKeycloakExampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
