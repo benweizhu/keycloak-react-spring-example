867a15c329faa97cc64d469cd6bc8619814e9ad4
benweizhu <benwei.zhu@exxonmobil.com>
2020-05-02 12:01:38 +0800

    can authorize api based on role

--- spring-security-adapter-keycloak-example/src/main/java/me/zeph/example/keycloak/security/springsecurityadapterkeycloakexample/SecurityConfig.java
+++ spring-security-adapter-keycloak-example/src/main/java/me/zeph/example/keycloak/security/springsecurityadapterkeycloakexample/SecurityConfig.java
@@ -1,5 +1,9 @@
 package me.zeph.example.keycloak.security.springsecurityadapterkeycloakexample;
 
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +12,8 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+import java.io.InputStream;
 
 @KeycloakConfiguration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
@@ -33,5 +39,39 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         .antMatchers("/public*").permitAll()
         .antMatchers("/private*").hasRole("USER")
         .anyRequest().permitAll();
+  }
+
+  /**
+   * Overrides default keycloak config resolver behaviour (/WEB-INF/keycloak.json) by a simple mechanism.
+   * <p>
+   * This example loads other-keycloak.json when the parameter use.other is set to true, e.g.:
+   * {@code ./gradlew bootRun -Duse.other=true}
+   *
+   * @return keycloak config resolver
+   */
+  @Bean
+  public KeycloakConfigResolver keycloakConfigResolver() {
+    return new KeycloakConfigResolver() {
+
+      private KeycloakDeployment keycloakDeployment;
+
+      @Override
+      public KeycloakDeployment resolve(HttpFacade.Request facade) {
+        if (keycloakDeployment != null) {
+          return keycloakDeployment;
+        }
+
+        String path = "/keycloak.json";
+        InputStream configInputStream = getClass().getResourceAsStream(path);
+
+        if (configInputStream == null) {
+          throw new RuntimeException("Could not load Keycloak deployment info: " + path);
+        } else {
+          keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+        }
+
+        return keycloakDeployment;
+      }
+    };
   }
 }
