a17eef9c397250c0d96a6c3c75e078d06b62b457
benweizhu <benwei.zhu@exxonmobil.com>
2020-05-04 14:02:20 +0800

    use-resource-role-mappings usage in spring security adapter

--- spring-security-adapter-keycloak-example/README.md
+++ spring-security-adapter-keycloak-example/README.md
@@ -1,7 +1,19 @@
 ## Naming Security Roles
 
+**https://www.keycloak.org/docs/latest/securing_apps/index.html#naming-security-roles**
+
 Spring Security, when using role-based authentication, requires that role names start with ROLE_. For example, an administrator role must be declared in Keycloak as ROLE_ADMIN or similar, not simply ADMIN.
 
-Realm Role
 
-Client Role(For audience)
+**https://www.keycloak.org/docs/latest/securing_apps/index.html#java-adapters**
+
+When use-resource-role-mappings to false
+
+Realm Role(For has role assertion)
+
+Client Role(For audience verification)
+
+When use-resource-role-mappings to true
+
+The adapter will look inside the token for application level role mappings for the user.
+
--- spring-security-adapter-keycloak-example/src/main/java/me/zeph/example/keycloak/security/springsecurityadapterkeycloakexample/ApiController.java
+++ spring-security-adapter-keycloak-example/src/main/java/me/zeph/example/keycloak/security/springsecurityadapterkeycloakexample/ApiController.java
@@ -2,6 +2,7 @@ package me.zeph.example.keycloak.security.springsecurityadapterkeycloakexample;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,8 @@ public class ApiController {
   }
 
   @GetMapping(value = "/private")
-  public ResponseEntity privateApi() {
-    return new ResponseEntity("This is private api", HttpStatus.OK);
+  public ResponseEntity privateApi(Authentication authentication) {
+    return new ResponseEntity("This is private api from " + authentication.getPrincipal() , HttpStatus.OK);
   }
 
 }
